0441901978bb6e61bcc2e3e3f3baec519c6b7c16
sofiaje <sofia.jespersen@hotmail.com>
2022-10-18 12:35:25 +0200

    removed startingpage

--- script.js
+++ script.js
@@ -19,89 +19,114 @@ let questions = [
         correctAnswer: "sant"
     },
     {
-        question: "Ja bor i Kalmar",
-        a: "sant",
-        b: "falskt",
-        correctAnswer: "falskt"
-    },
-    {
-        question: "Jag föddes igår",
-        a: "sant",
-        b: "falskt",
-        correctAnswer: "falskt"
-    },
-    {
-        question: "Mamma heter Anneli",
-        a: "sant",
-        b: "falskt",
-        correctAnswer: "sant"
-    },
-    {
-        question: "Jorden är platt",
-        a: "sant",
-        b: "falskt",
-        correctAnswer: "falskt"
-    },
-    {
-        question: "Ödlor styr världen",
+        question: "Jag bor i Kalmar",
         a: "sant",
         b: "falskt",
         correctAnswer: "falskt"
     }
+    // ,
+    // {
+    //     question: "Jag föddes igår",
+    //     a: "sant",
+    //     b: "falskt",
+    //     correctAnswer: "falskt"
+    // },
+    // {
+    //     question: "Mamma heter Anneli",
+    //     a: "sant",
+    //     b: "falskt",
+    //     correctAnswer: "sant"
+    // },
+    // {
+    //     question: "Jorden är platt",
+    //     a: "sant",
+    //     b: "falskt",
+    //     correctAnswer: "falskt"
+    // },
+    // {
+    //     question: "Ödlor styr världen",
+    //     a: "sant",
+    //     b: "falskt",
+    //     correctAnswer: "falskt"
+    // },
+    // {
+    //     question: "Hades bor i Hades",
+    //     a: "sant",
+    //     b: "falskt",
+    //     correctAnswer: "sant"
+    // },
+    // {
+    //     question: "Perseiderna dyker upp i juli",
+    //     a: "sant",
+    //     b: "falskt",
+    //     correctAnswer: "falskt"
+    // },
+    // {
+    //     question: "Gräset är grönt",
+    //     a: "sant",
+    //     b: "falskt",
+    //     correctAnswer: "sant"
+    // }
 ];
 
 let container = get("#container");
+let scoreH3 = create("h3");
 let submitBtn = create("button");
 submitBtn.innerText = "Kolla svar";
-let scoreH3 = create("h3");
 let radioBtnValue = [];
 counter = 0;
 
-let writeQuestions = (arr) => {
-    startBtn.remove();
-    arr.forEach((item, i) => {
-        let questionDiv = create("div");
-        questionDiv.classList.add("question-div");
-        let questionH3 = create("h3");
-        let list = create("ul");
-        questionH3.innerText = item.question;
+
+
+questions.forEach((item, i) => {
+    let questionDiv = create("div");
+    questionDiv.classList.add("question-div");
+    let questionH3 = create("h3");
+    let list = create("ul");
+    questionH3.innerText = item.question;
         
-        let liA = create("li");
-        liA.innerHTML = `<input type="radio" name="question${i}" id="${item.a}${i}" value="${item.a}">
-        <label for="${item.a}${i}">${item.a}</label>`;
+    let liA = create("li");
+    liA.innerHTML = `<input type="radio" name="question${i}" id="${item.a}${i}" value="${item.a}">
+    <label for="${item.a}${i}">${item.a}</label>`;
         
-        let liB = create("li");
-        liB.innerHTML = `<input type="radio" name="question${i}" id="${item.b}${i}" value="${item.b}">
-        <label for="${item.b}${i}">${item.b}</label>`;
+    let liB = create("li");
+    liB.innerHTML = `<input type="radio" name="question${i}" id="${item.b}${i}" value="${item.b}">
+    <label for="${item.b}${i}">${item.b}</label>`;
     
-        container.append(questionDiv);
-        questionDiv.append(questionH3, list);
-        list.append(liA, liB);
-    })    
+    container.append(questionDiv);
+    questionDiv.append(questionH3, list);
+    list.append(liA, liB);
+})    
     
-    container.append(submitBtn, scoreH3);
-}
+container.append(submitBtn, scoreH3);
 
 
-let startBtn = get("#start");
-startBtn.addEventListener("click", () => {
-    writeQuestions(questions);
-})
-
-submitBtn.addEventListener("click", () => {
-    if (document.querySelectorAll("[type='radio']:checked").length < questions.length) {
+let checkAnswers = (arr) => {
+    if (document.querySelectorAll("[type='radio']:checked").length < arr.length) {
         scoreH3.innerText = "Du har inte fyllt i alla svaren."
     } else {
-        scoreH3.innerText = "Ja, hurra!"
         let radioBtnAnswers = document.querySelectorAll("[type='radio']:checked");
         radioBtnAnswers.forEach(item => {
             radioBtnValue.push(item.value);
         })
         radioBtnValue.forEach((item, i) => {
-            if (item === questions[i].correctAnswer) {
+            let q = get(`[name='question${i}']:checked`);
+
+            if (item === arr[i].correctAnswer) {
                 counter++;
-            };
+                q.style.accentColor = "green";
+                q.nextElementSibling.style.color = "green";
+            } else {
+                q.style.accentColor = "red";
+                q.nextElementSibling.style.color = "red";
+            }
+            ;
         })
-        scoreH3.innerText = `du fick ${counter} rätt av 7 möjliga`;
+        scoreH3.innerText = `Du fick ${counter} rätt av ${arr.length} möjliga`;
     }
+}
+
+
+submitBtn.addEventListener("click", () => {
+    checkAnswers(questions);
 })
