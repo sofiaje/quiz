8f58857d3ea07f9760ee39bc5cfce76f196305c4
sofiaje <sofia.jespersen@hotmail.com>
2022-10-18 11:39:45 +0200

    check answer button

--- script.js
+++ script.js
@@ -5,61 +5,61 @@ let create = (x) => {
     return document.createElement(x);
 }
 
-let container = get("#container");
-
-
 let questions = [
     {
         question: "Grön är min bästa färg",
-        a: true,
-        b: false,
-        correctAnswer: true
+        a: "sant",
+        b: "falskt",
+        correctAnswer: "sant"
     },
     {
         question: "Jag har två ben",
-        a: true,
-        b: false,
-        correctAnswer: true
+        a: "sant",
+        b: "falskt",
+        correctAnswer: "sant"
     },
     {
         question: "Ja bor i Kalmar",
-        a: true,
-        b: false,
-        correctAnswer: false
+        a: "sant",
+        b: "falskt",
+        correctAnswer: "falskt"
     },
     {
         question: "Jag föddes igår",
-        a: true,
-        b: false,
-        correctAnswer: false
+        a: "sant",
+        b: "falskt",
+        correctAnswer: "falskt"
     },
     {
         question: "Mamma heter Anneli",
-        a: true,
-        b: false,
-        correctAnswer: true
+        a: "sant",
+        b: "falskt",
+        correctAnswer: "sant"
     },
     {
         question: "Jorden är platt",
-        a: true,
-        b: false,
-        correctAnswer: false
+        a: "sant",
+        b: "falskt",
+        correctAnswer: "falskt"
     },
     {
         question: "Ödlor styr världen",
-        a: true,
-        b: false,
-        correctAnswer: false
+        a: "sant",
+        b: "falskt",
+        correctAnswer: "falskt"
     }
 ];
+
+let container = get("#container");
 let submitBtn = create("button");
 submitBtn.innerText = "Kolla svar";
-    
+let scoreH3 = create("h3");
+let radioBtnValue = [];
+counter = 0;
 
 let writeQuestions = (arr) => {
     startBtn.remove();
     arr.forEach((item, i) => {
-        console.log(i);
         let questionDiv = create("div");
         questionDiv.classList.add("question-div");
         let questionH3 = create("h3");
@@ -67,20 +67,19 @@ let writeQuestions = (arr) => {
         questionH3.innerText = item.question;
         
         let liA = create("li");
-        liA.innerHTML = `<input type="radio" name="question${i}" id="${item.a}${i}">
+        liA.innerHTML = `<input type="radio" name="question${i}" id="${item.a}${i}" value="${item.a}">
         <label for="${item.a}${i}">${item.a}</label>`;
         
         let liB = create("li");
-        liB.innerHTML = `<input type="radio" name="question${i}" id="${item.b}${i}">
+        liB.innerHTML = `<input type="radio" name="question${i}" id="${item.b}${i}" value="${item.b}">
         <label for="${item.b}${i}">${item.b}</label>`;
-    
     
         container.append(questionDiv);
         questionDiv.append(questionH3, list);
         list.append(liA, liB);
     })    
     
-    container.append(submitBtn);
+    container.append(submitBtn, scoreH3);
 }
 
 
@@ -90,5 +89,19 @@ startBtn.addEventListener("click", () => {
 })
 
 submitBtn.addEventListener("click", () => {
-    console.log("jag klickar")
+    if (document.querySelectorAll("[type='radio']:checked").length < questions.length) {
+        scoreH3.innerText = "Du har inte fyllt i alla svaren."
+    } else {
+        scoreH3.innerText = "Ja, hurra!"
+        let radioBtnAnswers = document.querySelectorAll("[type='radio']:checked");
+        radioBtnAnswers.forEach(item => {
+            radioBtnValue.push(item.value);
+        })
+        radioBtnValue.forEach((item, i) => {
+            if (item === questions[i].correctAnswer) {
+                counter++;
+            };
+        })
+        scoreH3.innerText = `du fick ${counter} rätt av 7 möjliga`;
+    }
 })
