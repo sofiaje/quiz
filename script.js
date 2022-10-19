dd82c712ee1937ea2fa1c59de1e7eb1e8a8c8186
sofiaje <sofia.jespersen@hotmail.com>
2022-10-19 15:54:57 +0200

    changed array structure

--- script.js
+++ script.js
@@ -6,86 +6,57 @@ let create = (x) => {
 }
 
 let questions = [
-    // {
-    //     question: "Grön är min bästa färg",
-    //     a: "sant",
-    //     b: "falskt",
-    //     correctAnswer: "sant"
-    // },
-    // {
-    //     question: "Jag har två ben",
-    //     a: "sant",
-    //     b: "falskt",
-    //     correctAnswer: "sant"
-    // },
-    // {
-    //     question: "Jag bor i Kalmar",
-    //     a: "sant",
-    //     b: "falskt",
-    //     correctAnswer: "falskt"
-    // },
-    // {
-    //     question: "Jag föddes igår",
-    //     a: "sant",
-    //     b: "falskt",
-    //     correctAnswer: "falskt"
-    // },
     {
-        question: "Vad heter mamma?",
-        type: 2,
-        a: "Anna",
-        b: "Anneli",
-        c: "Lovisa",
-        d: "Silivia",
-        correctAnswer: "Anneli"
+        question: "Vilken färg är bäst?",
+        answers: ["Grön", "Gul", "Lila", "Turkos", "Blå", "rosa"],
+        correctAnswer: "Grön"
     },
-    // {
-    //     question: "Jorden är platt",
-    //     a: "sant",
-    //     b: "falskt",
-    //     correctAnswer: "falskt"
-    // },
     {
+        question: "Jag har två ben",
+        answers: ["Sant", "Falskt"],
+        correctAnswer: "Sant"
+    },
+    {
+        question: "Jag bor i Kalmar",
+        answers: ["Sant", "Falskt"],
+        correctAnswer: "Falskt"
+    },
+        {
         question: "Vem styr världen?",
-        type: 2,
-        a: "Sofia",
-        b: "Ödlor",
-        c: "Elon Musk",
-        d: "Ingen",
+        answers: ["Sofia", "Ödlor", "Elon Musk", "Ingen"],
         correctAnswer: "Ödlor"
     },
     {
         question: "Hades bor i Hades",
-        type: 1,
-        a: "sant",
-        b: "falskt",
-        correctAnswer: "sant"
+        answers: ["Sant", "Falskt"],
+        correctAnswer: "Sant"
     },
     {
         question: "Perseiderna dyker upp i juli",
-        type: 1,
-        a: "sant",
-        b: "falskt",
-        correctAnswer: "falskt"
+        answers: ["Sant", "Falskt"],
+        correctAnswer: "Falskt"
+    },
+    {
+        question: "Vad dricker Fantomen helst?",
+        answers: ["Cola", "Vatten", "Kaffe", "Mjölk"],
+        correctAnswer: "Mjölk"
+    },
+    {
+        question: "Vilken metod ska jag använda om jag vill lägga till ett element i början av en array?",
+        answers: ["pop", "push", "shift", "unshift"],
+        correctAnswer: "unshift"
     },
     {
         question: "Gräset är grönt",
-        type: 1,
-        a: "sant",
-        b: "falskt",
-        correctAnswer: "sant"
+        answers: ["Sant", "Falskt"],
+        correctAnswer: "Sant"
     },
     {
         question: "Vilket år blev olof palme mördad?",
-        type: 2,
-        a: "1985",
-        b: "1986",
-        c: "1987",
-        d: "1988",
+        answers: ["1985", "1986", "1987", "1988"],
         correctAnswer: "1986"
     }
 ];
-
 
 //variables
 let backgroundBtn = get(".backgroundBtn");
@@ -96,40 +67,31 @@ submitBtn.innerText = "Kolla svar";
 let radioBtnValue = [];
 counter = 0;
  
-
-let alternative = (item, i) => {
+//funktion som skapar svars-alternativ
+let createLi = (item, i) => {
     let li = create("li");
     li.innerHTML = `<input type="radio" name="question${i}" id="${item}${i}" value="${item}">
     <label for="${item}${i}">${item}</label>`;
     return li;
 }
 
-questions.forEach((item, i) => {
+//skapar en div och en ul för varje fråga
+//appendar frågor och svarsalternativ
+questions.forEach((question, i) => {
     let questionDiv = create("div");
     questionDiv.classList.add("question-div");
     let questionH3 = create("h3");
     let list = create("ul");
-    questionH3.innerText = item.question;
+    questionH3.innerText = question.question;
+    container.append(questionDiv);
+    questionDiv.append(questionH3, list);
 
-    if (item.type === 1) {
-        container.append(questionDiv);
-        questionDiv.append(questionH3, list);
-        
-        let liA = alternative(item.a, i);
-        let liB = alternative(item.b, i);
-        list.append(liA, liB);
+    question.answers.forEach(answer => {
+        let li = createLi(answer, i);
+        list.append(li);
+    })
+})
 
-    } else if (item.type === 2) {
-        let liA = alternative(item.a, i);
-        let liB = alternative(item.b, i);
-        let liC = alternative(item.c, i);
-        let liD = alternative(item.d, i);
-        
-        container.append(questionDiv);
-        questionDiv.append(questionH3, list);
-        list.append(liA, liB, liC, liD);
-    }
-})    
 container.append(submitBtn, scoreH4);
 
 
@@ -151,6 +113,7 @@ let checkAnswers = (arr) => {
             } else {
                 q.style.accentColor = "FireBrick";
                 q.nextElementSibling.innerHTML += ` `;
+
             }
         })
         scoreResult(scoreH4, arr);
@@ -172,7 +135,6 @@ let scoreResult = (score, arr) => {
     }
 }
 
-
 submitBtn.addEventListener("click", () => {
     checkAnswers(questions);
 })
@@ -181,6 +143,10 @@ submitBtn.addEventListener("click", () => {
 backgroundBtn.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
     container.classList.toggle("dark-mode-container");
+    let labels = document.querySelectorAll("label");
+    labels.forEach(item => {
+        item.classList.toggle("dark-mode-label")
+    })
     let buttons = document.querySelectorAll("button");
     buttons.forEach(item => {
         item.classList.toggle("button-dark-mode")
